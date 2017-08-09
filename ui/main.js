a73480a7a58d67bacb61af789b07160133ac670f
mjsirfan2015 <mohammedirfan2012india@gmail.com>
2017-08-09 14:24:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,7 @@ console.log('Loaded!');
 //gets element by their id
 var element=document.getElementById("main-text");
 element.innerHTML="New Value";
+var img=document.getElementById("madi");
+img.onclick=function(){
+    img.style.leftMargin="100px";
+};
