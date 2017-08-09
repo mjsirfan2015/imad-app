97485c50a3b0553e3866529732439da2fa97fd76
mjsirfan2015 <mohammedirfan2012india@gmail.com>
2017-08-09 14:30:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,12 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="New Value";
 var img=document.getElementById("madi");
+var marginLeft=100
+function movRight()
+{
+    marginLeft+=1;
+    img.style.marginLeft=marginLeft+"px";
+}
 img.onclick=function(){
-    img.style.marginLeft="100px";
+    movinterval= setInterval(movRight,100);
 };
