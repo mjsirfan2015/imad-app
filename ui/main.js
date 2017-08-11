3da7f785e9f33915f28c4659329fc69b48ed73c0
mjsirfan2015 <mohammedirfan2012india@gmail.com>
2017-08-12 02:17:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,4 +22,21 @@ button.onclick =function(){
     //Make a Request
     request.open("GET","http://mohammedirfan2012india.imad.hasura-app.io/counter",true);
     request.send(null);
+    
+    var nameinput=document.getElementById("name");
+    var name=nameinput.value;
+    var submit=document.getElementById("submit_btn");
+    var namelist=document.getElementById("namelist");
+    submit.onclick=function(){
+    //Make request t the server and capture the name
+    //Capture list of names and render it as list
+    var names=['name1','name2','name3'];
+    list=' '
+    for(var i=0;i<names.length;i++)
+    {
+        list+='<li>'+names[i]+'</li>';
+        namelist.innerHTML=list;
+        
+    }
+    } 
 }
