4777a84e309c18ba8410c6a6232f25d763ca132a
mjsirfan2015 <mohammedirfan2012india@gmail.com>
2017-08-12 02:48:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,19 +23,34 @@ button.onclick =function(){
     request.open("GET","http://mohammedirfan2012india.imad.hasura-app.io/counter",true);
     request.send(null);
 };
-    var nameinput=document.getElementById("name");
-    var name=nameinput.value;
+   
     var submit=document.getElementById("submit_btn");
     var namelist=document.getElementById("namelist");
-    submit.onclick=function(){
-    //Make request t the server and capture the name
-    //Capture list of names and render it as list
-    var names=['name1','name2','name3'];
-    list=' '
-    for(var i=0;i<names.length;i++)
-    {
-        list+='<li>'+names[i]+'</li>';
-        namelist.innerHTML=list;
+    submit.onclick= function(){
+   //make a request to counter endpoint
+    var request= new XMLHttpRequest();
+    
+    //Create a Request object
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHttpRequest.DONE)
+        {
+            //Take some Action
+            if(request.status===200)
+            {
+                var names=['name1','name2','name3'];
+                list=' '
+                for(var i=0;i<names.length;i++)
+                {       
+                 list+='<li>'+names[i]+'</li>';
+                 namelist.innerHTML=list;
         
+            }
+            }
+        }
     }
+        //Not done
+     var nameinput=document.getElementById("name");
+     var name=nameinput.value;
+    request.open("GET","http://mohammedirfan2012india.imad.hasura-app.io/submit-name?name="+name,true);
+    request.send(null);
     } ;
